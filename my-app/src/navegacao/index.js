86661af6de5c1959031dc93c4300281fc734cf84
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-09-11 18:41:41 -0300

    my app react

---
+++ my-app/src/navegacao/index.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {Card, Button , Container, Row, Col, Jumbotron} from 'react-bootrap';
+/* 
+    import futebol from '../...png'
+    import Style from './index.css'
+*/
+
+class Eventos extends Components{
+
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+
+    }
+
+    render(){
+        return(
+
+
+
+
+
+
+
+
+
+        )
+
+    }
+
+
+
+}
